6a726548a871cb5a0ef919ba0463aa711e174983
Николай Кузьмин <n.kuzmin@anserem.com>
2022-04-12 21:17:43 +0300

    Вынесение логики обработки push-сообщения в отдельный класс

--- GitUp/Controllers/GitlabController.cs
+++ GitUp/Controllers/GitlabController.cs
@@ -1,77 +1,33 @@
 ﻿using System;
-using System.Text.RegularExpressions;
 using GitUp.Interfaces;
-using GitUp.Models.Clickup;
 using GitUp.Models.Gitlab;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace GitUp.Controllers
 {
-    [ApiController]
+	[ApiController]
 	[Route("api/v1/[controller]/push")]
 	public class GitlabController : ControllerBase
 	{
-		// private readonly ILogger<GitlabController> _logger;
-		//
-		// public GitlabController(ILogger<GitlabController> logger)
-		// {
-		// 	_logger = logger;
-		// }
+		private readonly ILogger<GitlabController> _logger;
+		private readonly IConfiguration _config;
+
+		public GitlabController(ILogger<GitlabController> logger, IConfiguration config)
+		{
+			_logger = logger;
+			_config = config;
+		}
 
 		[HttpPost]
         [AcceptVerbs("POST")]
         public string Post([FromBody] WebHook webHook)
 		{
-			IWebHookHandler handler = new ClickUpWebHookHandler();
+			IWebHookHandler handler = new ClickUpWebHookHandler(_config["ClickUpAccessToken"]);
 			handler.HandlePushEvent(webHook);
 			
 			return String.Empty;
-		}
-	}
-
-	public class ClickUpWebHookHandler : IWebHookHandler
-	{
-		public void HandlePushEvent(WebHook webHook)
-		{
-			foreach (var commit in webHook.Commits)
-			{
-				string taskIdWithBrackets = GetTaskIdFromCommitMessage(commit);
-				string commitInfo = commit.Message.Replace(taskIdWithBrackets, string.Empty) + "   " + commit.Url;
-
-				string taskId = taskIdWithBrackets.Replace("[", String.Empty).Replace("]", String.Empty);
-				AddCheckListItemToTask(taskId, "Commits", commitInfo);
-			}
-		}
-
-		private string GetTaskIdFromCommitMessage(Commit commit)
-		{
-			string pattern = @"\[(.*)\]";
-			Regex rg = new Regex(pattern); 
-			
-			MatchCollection matchedStrings = rg.Matches(commit.Message);
-
-			return matchedStrings.Count > 0 ? matchedStrings[0].Value : string.Empty;
-		}
-		
-		private void AddCheckListItemToTask(string taskId, string checkListName, string checkListItemName)
-		{
-			string apiToken = "pk_8970047_OV7BUHRYTTCP0MRVEZYS8EC88S6IQVIO";
-			
-			ClickUpApiClient clickup = new ClickUpApiClient(apiToken);
-			Task task = clickup.GetTask(taskId);
-			if (task == null)
-			{
-				throw new Exception("Задача не была найдена");
-			}
-            
-			Checklist checklist = task.GetChecklist(checkListName);
-			if (checklist == null)
-			{
-				throw new Exception("Чеклист не был найден");
-			}
-            
-			clickup.CreateCheckListItem(checklist.Id, checkListItemName, task.Assignees[0]);
-            
 		}
 	}
 }
---
+++ GitUp/Models/Gitlab/ClickUpWebHookHandler.cs
@@ -0,0 +1,56 @@
+﻿using GitUp.Controllers;
+using GitUp.Interfaces;
+using GitUp.Models.Clickup;
+using System;
+using System.Text.RegularExpressions;
+
+namespace GitUp.Models.Gitlab
+{
+	public class ClickUpWebHookHandler : IWebHookHandler
+	{
+		private readonly string ApiToken;
+		public ClickUpWebHookHandler(string apiToken)
+		{
+			ApiToken = apiToken;
+		}
+
+
+		public void HandlePushEvent(WebHook webHook)
+		{
+			string checkListName = "Commits";
+			foreach (var commit in webHook.Commits)
+			{
+				string taskIdWithBrackets = GetTaskIdFromCommitMessage(commit);
+				string commitInfo = commit.Message.Replace(taskIdWithBrackets, string.Empty) + "   " + commit.Url;
+
+				string taskId = taskIdWithBrackets.Replace("[", String.Empty).Replace("]", String.Empty);
+				Checklist checkList = CreateChecklistIfNotExitsts(checkListName, taskId);
+				AddCheckListItemToTask(taskId, checkList.Name, commitInfo);
+			}
+		}
+
+		private Checklist CreateChecklistIfNotExitsts(string checkListName, string taskId)
+		{
+			IApiClient clickup = new ClickUpApiClient(ApiToken);
+			return clickup.CreateChecklistIfNotExitsts(taskId, checkListName);
+
+		}
+
+		private string GetTaskIdFromCommitMessage(Commit commit)
+		{
+			string pattern = @"\[(.*)\]";
+			Regex rg = new Regex(pattern);
+
+			MatchCollection matchedStrings = rg.Matches(commit.Message);
+
+			return matchedStrings.Count > 0 ? matchedStrings[0].Value : string.Empty;
+		}
+
+		private void AddCheckListItemToTask(string taskId, string checkListName, string checkListItemName)
+		{
+			IApiClient clickup = new ClickUpApiClient(ApiToken);
+			clickup.AddCheckListItemToTask(taskId, checkListName, checkListItemName);
+		}
+
+	}
+}
